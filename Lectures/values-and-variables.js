80fe338269419774415cb0cee245890c8731de7e
harshparab <harsh_parab@outlook.com>
2022-10-10 09:07:18 +0530

    finished values and variables

---
+++ Lectures/values-and-variables.js
@@ -0,0 +1,9 @@
+//Declaring a variable
+let firstName = "Harsh";
+let lastName = "Parab";
+let age = 24;
+
+// logging variables;
+console.log("My first name is", firstName);
+console.log("My last name is", lastName);
+console.log("My age is", age);
